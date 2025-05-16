e1738ae8f7531bb35be4eec0e0536561ebfb6376
umutcangokhan <umutcangokhan41@gmail.com>
2025-05-16 23:58:29 +0300

    API Denemesi
    api ile favori oyunlarımı siteye yüklemeyi denedim.

---
+++ web proje/js/script.js
@@ -0,0 +1,11 @@
+const API_KEY = "cbcc8f8e3dc747739f2f6f18d6366340";
+const RDR2_ID = 28; // Red Dead Redemption 2'nin RAWG ID'si
+
+fetch(`https://api.rawg.io/api/games/28?key=cbcc8f8e3dc747739f2f6f18d6366340`)
+  .then(r => r.json())
+  .then(game => {
+    document.getElementById('games-container').innerHTML = `
+      <img src="${game.background_image}" class="img-fluid rounded">
+      <h4 class="mt-3">${game.name}</h4>
+    `;
+  });
